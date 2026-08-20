d768af3bed4d6ef2581ef1608b86d4cd9b108435
Armaan Sharma <armaan192004@gmail.com>
2026-08-19 21:30:36 -0700

    deploy action

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+const repo = "Locked-In-Web";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+
+  basePath: isProd ? `/${repo}` : "",
+  assetPrefix: isProd ? `/${repo}/` : "",
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
